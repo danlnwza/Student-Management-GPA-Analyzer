37d4742c7461062e81d9866d08bf8bebc527d5ea
lab6 <chaidan254905@gmail.com>
2026-02-24 20:41:28 +0700

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include "StudentManager.h"
+
 using namespace std;
 
-int cal(int );
+int main() {
+    StudentManager manager;
+    int choice;
 
-int main(void) 
-{
-    //test
-    cout <<"test"<<endl;
-    cout<<cal(56);
+    do {
+        
+        cout << "\n====== Student Management System ======\n";
+        cout << "1. Add Student\n";
+        cout << "2. Show All Students\n";
+        cout << "3. Show Top 3 GPA\n";
+        cout << "4. Show Class Average GPA\n";
+        cout << "5. Save to File\n";
+        cout << "6. Load from File\n";
+        cout << "0. Exit\n";
+        cout << "Choose: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: manager.addStudent(); break;
+            case 2: manager.showAllStudents(); break;
+            case 3: manager.showTop3(); break;
+            case 4: manager.showAverageGPA(); break;
+            case 5: manager.saveToFile(); break;
+            case 6: manager.loadFromFile(); break;
+            case 0: cout << "sawaddee!\n"; break;
+            default: cout << "Invalid choice.\n";
+        }
+
+    } while (choice != 0);
+
     return 0;
-}
-
-int cal(int x){
-    x+=26;
-    return x;
 }
