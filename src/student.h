50cf1bc182a0c2fa73b5164d19776f1ddb4a9ffc
lab6 <chaidan254905@gmail.com>
2026-03-10 17:27:55 +0700

    student.h

--- src/student.h
+++ src/student.h
@@ -1,29 +1,42 @@
+/*
+ขั้นตอนที่ 1.1 สร้าง class student ในการกําหนด member ต่างๆ (Blueprint ของนักเรียน 1 คน) เพื่อใช้ในการจัดการข้อมูลใน student.cpp
+*/
+
+//include guard ป้องกัน include ซํ้า
 #ifndef STUDENT_H
 #define STUDENT_H
 
 #include <string>
-
 using namespace std;
 
-class Student {
+//ใช้ความรู้ในเรื่องของ class 
 
-private:
-
-    string id,name;
+class student {
+//ขั้นตอนที่ 1.1 ฐานข้อมูลระบบ id name gpa
+    private:
+    string id;
+    string name;
     double gpa;
 
-public:
+//Constructor Overloading
+    public:
+    student();//Default Constructor objเปล่า
+    student(string id,string name,double gpa);// Parameterized Constructor พร้อม obj
 
+//เข้าถึงข้อมูล private ผ่าน set&get method
 
-    Student(string i, string n, double g);
+    void set_id(string id);
+    void set_name(string name);
+    void set_gpa(double gpa);
 
-    string getID() const;
+    string get_id();
+    string get_name();
+    double get_gpa();
+    
 
-    string getName() const;
-
-    double getGPA() const;
-
-    void display() const;
 };
 
 #endif
+
+
+
