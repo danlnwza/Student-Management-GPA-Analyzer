6f59cb62e1a34bb7309260bd6cf7eb06e8b71131
lab6 <chaidan254905@gmail.com>
2026-03-10 17:27:43 +0700

    student.cpp

--- src/student.cpp
+++ src/student.cpp
@@ -1,22 +1,49 @@
-#include "Student.h"
-#include <iostream>
-#include <iomanip>
+/*
+ขั้นตอนที่ 1.2 : เขียนการทำงานจริงของ class student ใน student.h
+-จัดการข้อมูลนักเรียนแต่ละคนแบบเรียวไทม์
+*/
 
-using namespace std;
+#include"student.h"//นําไฟล์ header student.h มาใช้
 
-Student::Student(string i, string n, double g) {
-    id = i;
-    name = n;
-    gpa = g;
+//defualt constructor เปล่า กําหนดค่าเริ่มต้นทุกครั้งในการสร้าง obj
+student::student(){
+    id="";
+    name="";
+    gpa=0;
 }
 
-string Student::getID() const { return id; }
-string Student::getName() const { return name; }
-double Student::getGPA() const { return gpa; }
+//parameter constructor สร้าง obj พร้อมข้อมูล
+student::student(string id,string name,double gpa){
+    //กําหนดค่า ผ่าน this pointer (เพื่อให้โปรแกรมไม่สับสน)
+    this->id=id; //id ของ obj = id (พารามิเตอร์ที่รับมา)
+    this->name=name;
+    this->gpa=gpa;
+}
 
-void Student::display() const {
-    
-    cout << left << setw(10) << id
-         << setw(20) << name
-         << fixed << setprecision(2) << gpa << endl;
+//Data abstraction
+
+//setter function แก้ไขค่า (set ค่าข้อมูล)
+void student::set_id(string id){
+    this->id=id;
+}
+
+void student::set_name(string name){
+    this->name=name;
+}
+
+void student::set_gpa(double gpa){
+    this->gpa=gpa;
+}
+
+//getter function ใช้ดึงค่าออดมาใช้ (get ค่าข้อมูล)
+string student::get_id(){
+    return id;//มีการส่งค่ากลับไป
+}
+
+string student::get_name(){
+    return name;
+}
+
+double student::get_gpa(){
+    return gpa;
 }
